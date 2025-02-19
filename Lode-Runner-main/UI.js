9c303c8660be661f09bd43622201bcdc1fd53b85
Alexandru Ciuca <alexandruciuca@Alexandrus-MacBook-Pro.local>
2025-02-19 14:46:34 -0500

    drew blk rect

--- Lode-Runner-main/UI.js
+++ Lode-Runner-main/UI.js
@@ -2,8 +2,16 @@ function initUI(){
     objC2D.fillStyle = "orange";
     objC2D.fillRect(0, 0, objCanvas.width, objCanvas.height);
 
-    var strText = "LODE RUNNER PAR ANDREI CRETU ET ALEXANDRU CIUCA";
+    var strText = "PAR ANDREI CRETU ET ALEXANDRU CIUCA";
     objC2D.font = "40px 'Press Start 2P'";
     objC2D.fillStyle = "black";
-    objC2D.fillText(strText, objCanvas.width / 2 - objC2D.measureText(strText).width / 2, objCanvas.height  - objCanvas.height / 6.3);
+    objC2D.fillText(strText, objCanvas.width / 2 - objC2D.measureText(strText).width / 2, objCanvas.height  - objCanvas.height / 30);
+
+    strText = "LODE RUNNER";
+    objC2D.font = "40px 'Press Start 2P'";
+    objC2D.fillStyle = "black";
+    objC2D.fillText(strText, objCanvas.width / 2 - objC2D.measureText(strText).width / 2, objCanvas.height  - objCanvas.height + 60);
+
+
+    objC2D.fillRect(50,objCanvas.height - 300, objCanvas.width - 100, 200);
 }
