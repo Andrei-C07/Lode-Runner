638e5ae40db643715bd6c1dad0d937fb5ca725f1
Alexandru Ciuca <alexandru356.c@gmail.com>
2025-02-23 21:18:21 -0500

    player movement

--- Lode-Runner-main/player.js
+++ Lode-Runner-main/player.js
@@ -2,18 +2,21 @@ function initPlayer(){
     objImgPlayer = new Image();
     objImgPlayer.src = "assets/player/playerIDLE.png";
     objPlayer = new Object();
-
+    //Onload pour executer la fonction draw() seulement quand l'image est chargée
+    objImgPlayer.onload = function(){
+        console.log("Player loaded");
+        draw();
+    };
     objPlayer.imgPlayer = objImgPlayer;
     objPlayer.playerIntX = spawnX;
     objPlayer.playerIntY = spawnY;
     objPlayer.playerSpeed = 10;
     objPlayer.playerDirection = 0;
-
-    console.log("Player initialized");
 }
 
 function drawPlayer(){
     objC2D.save();
+    
     objC2D.drawImage(objPlayer.imgPlayer, objPlayer.playerIntX, objPlayer.playerIntY);
     objC2D.restore();
 }
@@ -33,6 +36,19 @@ function movePlayer(){
     }
 
     if(binMoveable){
+        erasePlayer();
         objPlayer.playerIntX += objPlayer.playerSpeed * objPlayer.playerDirection;
+        redessinerMursMap();
+        drawPlayer();
     }
+
+}
+//redessiner les murs et la map car sinon le joueur laisse une trace et
+//si tu redessine tout le canvas, le timer va reset a chaque mouvement 
+function redessinerMursMap() {
+    objC2D.save();
+    objC2D.clearRect(objPlayer.playerIntX, objPlayer.playerIntY, objPlayer.width, objPlayer.height);
+    drawMap(); 
+    drawWalls(); 
+    objC2D.restore();
 }
