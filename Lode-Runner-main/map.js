4045754e08e4866d13328e2cd6a32c248b921418
Andrei Cretu <andrei.cretu4676@gmail.com>
2025-02-23 19:11:05 -0500

    structure changes and player can almost move left and right

--- Lode-Runner-main/map.js
+++ Lode-Runner-main/map.js
@@ -1,11 +1,7 @@
     function initMap(){
         
-        objC2D.translate(50, 100);
-        objC2D.fillStyle = "black";
-        objC2D.fillRect( 0, 0, objCanvas.width - 100, objCanvas.height - 400);
-        
-        let mapHeight = canvas.height - 400;
-        let mapWidth = canvas.width - 100;
+        mapHeight = canvas.height - 400;
+        mapWidth = canvas.width - 100;
 
         // v = void
         // b = brick
@@ -16,7 +12,7 @@
         // S = spawn
 
 
-        let map = [
+        map = [
             ["v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v"],
             ["v","v","v","v","g","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v","v"],
             ["b","b","b","b","b","b","b","l","b","b","b","b","b","b","b","v","v","v","v","v","v","v","v","v","v","v","v","v"],
@@ -35,179 +31,186 @@
             ["b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b","b"],
             ["p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p","p"]
         ];
-        
-        const voidTexture = new Image();
-        voidTexture.src = "./assets/textures/void.png";
-        
-        voidTexture.onload = () => {
-            console.log("Void texture loaded");
-            let drawVoid = () => {
-                for (let i = 0; i < map.length; i++){
-                    for(let j = 0; j < map[i].length; j++){
-                        if(map[i][j] == "v" || map[i][j] == "S"){
+          
+}
 
-                            if(map[i][j] == "S"){
-                                spawnX = j * (mapWidth / map[i].length);
-                                spawnY = i * (mapHeight / map.length);
-                            }
-                            
-                            const cellHeight = mapHeight / map.length;
-                            const cellWidth = mapWidth / map[i].length;
-                            objC2D.drawImage(voidTexture, j * cellWidth, i * cellHeight)
-                                
-                        }
-                    }
-                }
-            }
-        
-            drawVoid();
-        }
-        
-        const brickTexture = new Image();
-        brickTexture.src = "./assets/textures/brick.png";
-        
-        brickTexture.onload = () => {
-            console.log("Brick texture loaded");
-            let drawBrick = () => {
-                for (let i = 0; i < map.length; i++){
-                    for(let j = 0; j < map[i].length; j++){
-                        if(map[i][j] == "b"){
-                            const cellHeight = mapHeight / map.length;
-                            const cellWidth = mapWidth / map[i].length;
-                            objC2D.drawImage(brickTexture, j * cellWidth, i * cellHeight)
-                        }
-                    }
-                }
-            }
-        
-            drawBrick();
-        }
-        
-        const ladderTexture = new Image();
-        ladderTexture.src = "./assets/textures/ladder.png";
-        
-        ladderTexture.onload = () => {
-            console.log("Ladder texture loaded");
-            let drawLadder = () => {
-                for (let i = 0; i < map.length; i++){
-                    for(let j = 0; j < map[i].length; j++){
-                        if(map[i][j] == "l"){
-                            const cellHeight = mapHeight / map.length;
-                            const cellWidth = mapWidth / map[i].length;
-                            objC2D.drawImage(ladderTexture, j * cellWidth, i * cellHeight)
-                        }
-                    }
-                }
-            }
-            
-            drawLadder();
-        }
-        
-        const ropeTexture = new Image();
-        ropeTexture.src = "./assets/textures/rope.png";
-        
-        ropeTexture.onload = () => {
-            console.log("Rope texture loaded");
-            let drawRope = () => {
-                for (let i = 0; i < map.length; i++){
-                    for(let j = 0; j < map[i].length; j++){
-                        if(map[i][j] == "r"){
-                            const cellHeight = mapHeight / map.length;
-                            const cellWidth = mapWidth / map[i].length;
-                            objC2D.drawImage(ropeTexture, j * cellWidth, i * cellHeight)
-                        }
-                    }
-                }
-            }
-        
-            drawRope();
-        }
-        
-        const pavementTexture = new Image();
-        pavementTexture.src = "./assets/textures/pavement.png";
-        
-        pavementTexture.onload = () => {
-            console.log("Pavement texture loaded");
-            let drawPavement = () => {
-                for (let i = 0; i < map.length; i++){
-                    for(let j = 0; j < map[i].length; j++){
-                        if(map[i][j] == "p"){
-                            const cellHeight = mapHeight / map.length;
-                            const cellWidth = mapWidth / map[i].length;
-                            objC2D.drawImage(pavementTexture, j * cellWidth, i * cellHeight)
-                        }
-                    }
-                }
-            }
-        
-            drawPavement();
-        }
-        
-        const goldTexture = new Image();
-        goldTexture.src = "./assets/textures/gold.png";
-        
-        goldTexture.onload = () => {
-            console.log("Gold texture loaded");
-            let drawGold = () => {
-                for (let i = 0; i < map.length; i++){
+function drawMap(){
+    const offsetX = 50;
+    const offsetY = 100; 
+
+    objC2D.fillStyle = "black";
+    objC2D.fillRect(offsetX, offsetY, objCanvas.width - 100, objCanvas.height - 400);
+
+    const voidTexture = new Image();
+    voidTexture.src = "./assets/textures/void.png";
+
+    voidTexture.onload = () => {
+        console.log("Void texture loaded");
+        let drawVoid = () => {
+            for (let i = 0; i < map.length; i++){
                 for(let j = 0; j < map[i].length; j++){
-                    if(map[i][j] == "g"){
+                    if(map[i][j] == "v" || map[i][j] == "S"){
+
+                        // if(map[i][j] == "S"){
+                        //     spawnX = j * (mapWidth / map[i].length) + offsetX;
+                        //     spawnY = i * (mapHeight / map.length) + offsetY;
+                        //     console.log(spawnX, spawnY);
+                        // }
+                        
                         const cellHeight = mapHeight / map.length;
                         const cellWidth = mapWidth / map[i].length;
-                        objC2D.drawImage(goldTexture, j * cellWidth, i * cellHeight)
+                        objC2D.drawImage(voidTexture, j * cellWidth + offsetX, i * cellHeight + offsetY);
                     }
                 }
             }
         }
     
+        drawVoid();
+    }
+
+    const brickTexture = new Image();
+    brickTexture.src = "./assets/textures/brick.png";
+
+    brickTexture.onload = () => {
+        console.log("Brick texture loaded");
+        let drawBrick = () => {
+            for (let i = 0; i < map.length; i++){
+                for(let j = 0; j < map[i].length; j++){
+                    if(map[i][j] == "b"){
+                        const cellHeight = mapHeight / map.length;
+                        const cellWidth = mapWidth / map[i].length;
+                        objC2D.drawImage(brickTexture, j * cellWidth + offsetX, i * cellHeight + offsetY);
+                    }
+                }
+            }
+        }
+    
+        drawBrick();
+    }
+
+    const ladderTexture = new Image();
+    ladderTexture.src = "./assets/textures/ladder.png";
+
+    ladderTexture.onload = () => {
+        console.log("Ladder texture loaded");
+        let drawLadder = () => {
+            for (let i = 0; i < map.length; i++){
+                for(let j = 0; j < map[i].length; j++){
+                    if(map[i][j] == "l"){
+                        const cellHeight = mapHeight / map.length;
+                        const cellWidth = mapWidth / map[i].length;
+                        objC2D.drawImage(ladderTexture, j * cellWidth + offsetX, i * cellHeight + offsetY);
+                    }
+                }
+            }
+        }
+        
+        drawLadder();
+    }
+
+    const ropeTexture = new Image();
+    ropeTexture.src = "./assets/textures/rope.png";
+
+    ropeTexture.onload = () => {
+        console.log("Rope texture loaded");
+        let drawRope = () => {
+            for (let i = 0; i < map.length; i++){
+                for(let j = 0; j < map[i].length; j++){
+                    if(map[i][j] == "r"){
+                        const cellHeight = mapHeight / map.length;
+                        const cellWidth = mapWidth / map[i].length;
+                        objC2D.drawImage(ropeTexture, j * cellWidth + offsetX, i * cellHeight + offsetY);
+                    }
+                }
+            }
+        }
+    
+        drawRope();
+    }
+
+    const pavementTexture = new Image();
+    pavementTexture.src = "./assets/textures/pavement.png";
+
+    pavementTexture.onload = () => {
+        console.log("Pavement texture loaded");
+        let drawPavement = () => {
+            for (let i = 0; i < map.length; i++){
+                for(let j = 0; j < map[i].length; j++){
+                    if(map[i][j] == "p"){
+                        const cellHeight = mapHeight / map.length;
+                        const cellWidth = mapWidth / map[i].length;
+                        objC2D.drawImage(pavementTexture, j * cellWidth + offsetX, i * cellHeight + offsetY);
+                    }
+                }
+            }
+        }
+    
+        drawPavement();
+    }
+
+    const goldTexture = new Image();
+    goldTexture.src = "./assets/textures/gold.png";
+
+    goldTexture.onload = () => {
+        console.log("Gold texture loaded");
+        let drawGold = () => {
+            for (let i = 0; i < map.length; i++){
+                for(let j = 0; j < map[i].length; j++){
+                    if(map[i][j] == "g"){
+                        const cellHeight = mapHeight / map.length;
+                        const cellWidth = mapWidth / map[i].length;
+                        objC2D.drawImage(goldTexture, j * cellWidth + offsetX, i * cellHeight + offsetY);
+                    }
+                }
+            }
+        }
         drawGold();
-    }    
+    }  
+    console.log("Map initialized");
 }
+
 
 function initWalls(){
     tabObjWalls = new Array();
     var objWall = null;
 
-    //walls have color for testing
-
     //Mur de gauche
     objWall = new Object();
-    objWall.strColor = "green";  //keep color for now
-    objWall.intXStart = -10;
-    objWall.intYStart = 0;
-    objWall.intXEnd = 0;
-    objWall.intYEnd = objCanvas.height - 400;
+    objWall.intXStart = 40;
+    objWall.intYStart = 100;
+    objWall.intXEnd = 50;
+    objWall.intYEnd = objCanvas.height - 300;
     tabObjWalls.push(objWall);
 
     //Mur de droite
     objWall = new Object();
-    objWall.intXStart = objCanvas.width - 100;
-    objWall.intYStart = 0;
-    objWall.intXEnd = objCanvas.width - 90;
-    objWall.intYEnd = objCanvas.height - 400;
+    objWall.intXStart = objCanvas.width - 50;
+    objWall.intYStart = 100;
+    objWall.intXEnd = objCanvas.width - 40;
+    objWall.intYEnd = objCanvas.height - 300;
     tabObjWalls.push(objWall);
 
     //Mur du haut
     objWall = new Object();
-    objWall.intXStart = 0;
-    objWall.intYStart = -10;
-    objWall.intXEnd = objCanvas.width - 100;
-    objWall.intYEnd = 0;
+    objWall.intXStart = 50;
+    objWall.intYStart = 100;
+    objWall.intXEnd = objCanvas.width - 50;
+    objWall.intYEnd = 90;
     tabObjWalls.push(objWall);
 
     //Mur du bas
     objWall = new Object();
-    objWall.intXStart = 0;
-    objWall.intYStart = objCanvas.height - 400;
-    objWall.intXEnd = objCanvas.width - 100;
-    objWall.intYEnd = objCanvas.height - 390;
+    objWall.intXStart = 50;
+    objWall.intYStart = objCanvas.height - 300;
+    objWall.intXEnd = objCanvas.width - 50;
+    objWall.intYEnd = objCanvas.height - 290;
     tabObjWalls.push(objWall);
 }
 
 function drawWalls(){
     for(var i = 0; i < tabObjWalls.length; i++){
         var objWall = tabObjWalls[i];
-        objC2D.fillStyle = objWall.strColor;
+        objC2D.fillStyle = "green";
         objC2D.fillRect(objWall.intXStart, objWall.intYStart, objWall.intXEnd - objWall.intXStart, objWall.intYEnd - objWall.intYStart);
     }
 }
--- Lode-Runner-main/player.js
+++ Lode-Runner-main/player.js
@@ -1,24 +1,38 @@
 function initPlayer(){
-    player = new Object();
-    player = new Image();
-    player.src = "assets/player/playerIDLE.png";
-    player.onload = function() {
-        drawPlayer();
-    };
-    playerX = spawnX;
-    playerY = spawnY;
+    objImgPlayer = new Image();
+    objImgPlayer.src = "assets/player/playerIDLE.png";
+    objPlayer = new Object();
+
+    objPlayer.imgPlayer = objImgPlayer;
+    objPlayer.playerIntX = spawnX;
+    objPlayer.playerIntY = spawnY;
+    objPlayer.playerSpeed = 10;
+    objPlayer.playerDirection = 0;
+
+    console.log("Player initialized");
 }
 
 function drawPlayer(){
-    objC2D.drawImage(player, playerX, playerY);
-    //player = null;
+    objC2D.save();
+    objC2D.drawImage(objPlayer.imgPlayer, objPlayer.playerIntX, objPlayer.playerIntY);
+    objC2D.restore();
 }
 
-// function movePlayer(){
-//     switch (event.keyCode) {
-//         case 39: // Flèche-à-droite
-//             playerX += 10;
-//             drawPlayer();
-//             break;
-//     }
-// }
+function movePlayer(){
+    var binMoveable = true;
+    switch (event.keyCode) {
+        case 39 : // ->
+            objPlayer.playerDirection = 1;
+            console.log("bouge droite");
+            break;
+        case 37 : // <-
+            objPlayer.playerDirection = -1;
+            
+            console.log("bouge gauche");
+            break;
+    }
+
+    if(binMoveable){
+        objPlayer.playerIntX += objPlayer.playerSpeed * objPlayer.playerDirection;
+    }
+}
